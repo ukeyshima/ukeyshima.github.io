3ce4ea92f69f820ed7cb3526dbdf8b28067603d5
Yuki Shimada <ukeyshima@gmail.com>
2017-10-19 15:39:16 +0900

    Change of design

--- src/link.js
+++ src/link.js
@@ -1,51 +1,50 @@
-window.addEventListener("load",function(){
+window.addEventListener("load",()=>{
     let dir = location.href.split("/");
     dir = dir[dir.length -2];
-    let httpObj=new XMLHttpRequest();
+    const httpObj=new XMLHttpRequest();
     httpObj.open("get","../data/data.json",true);
-    httpObj.onload=function(){
-        let data=JSON.parse(this.responseText);
-        let title=document.createElement("p");
+    httpObj.addEventListener("load",function(){
+        const data=JSON.parse(this.responseText);
+        const title=document.createElement("p");
         title.id="title";
         document.body.appendChild(title);
         title.innerHTML=dir;
-        let check=document.createElement("input");
+        const check=document.createElement("input");
         check.id="check";
         check.type="checkbox";
         document.body.appendChild(check);
-        let label=document.createElement("label");
-        console.log(label);
+        const label=document.createElement("label");
         label.htmlFor="check";
         document.body.appendChild(label);
-        let link=document.createElement("div");
+        const link=document.createElement("div");
         link.id="link";
         label.appendChild(link);
-        let up=document.createElement("div");
+        const up=document.createElement("div");
         up.className="linkChild";
         up.id="up";
         document.body.appendChild(up);
-        let middle=document.createElement("div");
+        const middle=document.createElement("div");
         middle.className="linkChild";
         middle.id="middle";
         document.body.appendChild(middle);
-        let low=document.createElement("div");
+        const low=document.createElement("div");
         low.className="linkChild";
         low.id="low";
         document.body.appendChild(low);
-        let linkDiv=document.createElement("div");
+        const linkDiv=document.createElement("div");
         linkDiv.id="linkDiv";
         document.body.appendChild(linkDiv);
-        let titleLink=document.createElement("div");
+        const titleLink=document.createElement("div");
         titleLink.id="titleLink";
         linkDiv.appendChild(titleLink);
         titleLink.style.backgroundImage="url(\"../image/toppage.png\")";
         titleLink.style.top="100px";
-        let a=document.createElement("a");
+        const a=document.createElement("a");
         a.href="../";
         titleLink.appendChild(a);
         let count=0;
         for(key in data){
-            data[key].forEach(function(i){
+            data[key].forEach((i)=>{
                 if(i.name!=dir){
                   let link=document.createElement("div");
                   link.id=i.name;
@@ -59,7 +58,7 @@ window.addEventListener("load",function(){
                 }
             });
         }
-    }
+    });
     httpObj.send(null);
 
 });
--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
-window.addEventListener("load",function(){
-    let httpObj=new XMLHttpRequest();
+window.addEventListener("load",()=>{
+    const httpObj=new XMLHttpRequest();
     httpObj.open("get","../data/data.json",true);
-    httpObj.onload=function(){
-        let data=JSON.parse(this.responseText);
-        let works=document.getElementById("works");
+    httpObj.addEventListener("load",function(){
+        const data=JSON.parse(this.responseText);
+        const works=document.getElementById("works");
         for(key in data){
-            let title=document.createElement("h2");
+            const title=document.createElement("h2");
             title.className="title";
             works.appendChild(title);
-            title.innerHTML=key;
-            data[key].forEach(function(i){
-                let div=document.createElement("div");
+            title.innerHTML="#"+key;
+            data[key].forEach((i)=>{
+                const div=document.createElement("div");
                 div.id=i.name;
                 div.style.backgroundImage="url(image/"+i.name+".png)";
                 works.appendChild(div);
-                let a=document.createElement("a");
+                const a=document.createElement("a");
                 a.href=i.name+"/";
                 div.appendChild(a);
             });
         }
-    }
+    });
     httpObj.send(null);
 });
