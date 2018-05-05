7c563c3d30ff8115f247582920dfd9657ac1a3cd
Yuki Shimada <ukeyshima@gmail.com>
2018-05-05 13:45:38 +0900

    clean

--- src/cubeMapping/cubeFragmentShader.glsl
+++
@@ -1,8 +0,0 @@
-#version 300 es
-precision mediump float;
-uniform samplerCube cubeTexture;
-in vec3 vPosition;
-out vec4 outColor;
-void main(void){
-    outColor=texture(cubeTexture,vPosition);    
-}
