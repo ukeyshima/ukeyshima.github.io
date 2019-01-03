6d2ced22c3866486df8fc141274fc86f935848ec
Yuki Shimada <ukeyshima@gmail.com>
2019-01-03 23:26:11 +0900

    update

--- src/loading/vertexShader.glsl
+++
@@ -1,7 +0,0 @@
-attribute vec3 position;
-attribute vec2 textureCoord;
-varying vec2 vTextureCoord;
-void main(void){
-    vTextureCoord = textureCoord;
-    gl_Position = vec4(position, 1.0);
-}
--- src/workDescription.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class WorkDescription extends React.Component {
-  render() {
-    return <div className='workDescription'>{this.props.description}</div>;
-  }
-}
